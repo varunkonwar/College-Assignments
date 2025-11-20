26c13ff25d31db1e80dedf4ae1346ada175abf44
varunkonwar <179952644+varunkonwar@users.noreply.github.com>
2025-11-20 19:59:25 +0530

    Improved type checking design

--- 2nd Year/syQues_C++/1.cpp
+++ 2nd Year/syQues_C++/1.cpp
@@ -1,29 +1,31 @@
+// check type of triangle
 #include <iostream>
 using namespace std;
 class triangle{
-    float x, y, z;
-public:
-    triangle(float s1, float s2, float s3){
-        x=s1, y=s2, z=s3;  
-    }
-    int checktype(){
-        if (x == y && x != z)
-        {
-            return 1;
-        }
-        return 0;  
-    }
+    float s1, s2, s3;
+    public: 
+    triangle(float x, float y, float z):s1(x), s2(y), s3(z) {};
+
+    int checktype();
 };
+inline int triangle::checktype(){
+    if (s1 == s2 && s2 == s3 && s1 == s3)
+    {
+        return 0;
+    }else if (s1 != s2 && s2 != s3 && s3 != s1)
+    {
+        return 2;
+    }else
+        return 1;
+}
 int main(){
     float s1, s2, s3;
-    cout << "\nEnter the sides: ";
+    cout << "\nEnter the sides of the triangle: ";
     cin >> s1 >> s2 >> s3;
-    triangle checkme(s1, s2, s3);
+    triangle t1(s1, s2, s3);
 
-    if (checkme.checktype())
-    {
-        cout << "\nThe above inputs will form a isoceles triangle";
-    }else
-        cout << "\nTherefore the above inputs will form a equilateral triangle.";
+    string types[3] = {"equilateral", "isoceles", "scalene"};
+    cout << "\nThe above inputs will form " << types[t1.checktype()] << endl;
+
     return 0;
 }
